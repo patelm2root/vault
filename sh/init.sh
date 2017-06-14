3de6ce31978550c00389b4339fa038f2bb2faac4
Jonathan Zacharuk <jzacharuk@gmail.com>
2017-06-14 14:08:01 -0700

    Removed prespace before multi line sql

--- sh/init.sh
+++ sh/init.sh
@@ -7,7 +7,7 @@ set -e
 # transfer of data from the EMR to the Vault.
 # Note that the tally/adapter roles are created here rather than in presetup.sql so we can pass
 # through the TALLY_PASSWORD and ADAPTER_PASSWORD env variables.
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d postgres <<-EOSQL
   CREATE DATABASE vault;
   CREATE ROLE tally WITH LOGIN ENCRYPTED PASSWORD '$TALLY_PASSWORD';
   CREATE ROLE adapter WITH LOGIN ENCRYPTED PASSWORD '$ADAPTER_PASSWORD';
@@ -22,34 +22,34 @@ EOSQL
 
 # Run all of the sql scripts in specific order to create the schemas, tables, functions, etc.
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d vault \
- -f ./sql/presetup.sql \
- -f ./sql/admin/schema.sql \
- -f ./sql/admin/functions/analyze_db.sql \
- -f ./sql/admin/functions/verify_key.sql \
- -f ./sql/admin/functions/verify_trusted.sql \
- -f ./sql/admin/tables/trusted_keys.sql \
- -f ./sql/api/schema.sql \
- -f ./sql/api/functions/aggregate.sql \
- -f ./sql/api/functions/change.sql \
- -f ./sql/api/functions/prepare.sql \
- -f ./sql/api/functions/reset.sql \
- -f ./sql/api/functions/version.sql \
- -f ./sql/audit/schema.sql \
- -f ./sql/audit/tables/aggregate_log.sql \
- -f ./sql/audit/tables/change_log.sql \
- -f ./sql/concept/schema.sql \
- -f ./sql/indicator/schema.sql \
- -f ./sql/universal/schema.sql \
- -f ./sql/universal/tables/state.sql \
- -f ./sql/universal/tables/clinic.sql \
- -f ./sql/universal/tables/practitioner.sql \
- -f ./sql/universal/tables/patient.sql \
- -f ./sql/universal/tables/patient_practitioner.sql \
- -f ./sql/universal/tables/attribute.sql \
- -f ./sql/universal/tables/entry.sql \
- -f ./sql/universal/tables/entry_attribute.sql \
- -f ./sql/universal/data/attributes.sql \
- -f ./sql/postsetup.sql;
+-f ./sql/presetup.sql \
+-f ./sql/admin/schema.sql \
+-f ./sql/admin/functions/analyze_db.sql \
+-f ./sql/admin/functions/verify_key.sql \
+-f ./sql/admin/functions/verify_trusted.sql \
+-f ./sql/admin/tables/trusted_keys.sql \
+-f ./sql/api/schema.sql \
+-f ./sql/api/functions/aggregate.sql \
+-f ./sql/api/functions/change.sql \
+-f ./sql/api/functions/prepare.sql \
+-f ./sql/api/functions/reset.sql \
+-f ./sql/api/functions/version.sql \
+-f ./sql/audit/schema.sql \
+-f ./sql/audit/tables/aggregate_log.sql \
+-f ./sql/audit/tables/change_log.sql \
+-f ./sql/concept/schema.sql \
+-f ./sql/indicator/schema.sql \
+-f ./sql/universal/schema.sql \
+-f ./sql/universal/tables/state.sql \
+-f ./sql/universal/tables/clinic.sql \
+-f ./sql/universal/tables/practitioner.sql \
+-f ./sql/universal/tables/patient.sql \
+-f ./sql/universal/tables/patient_practitioner.sql \
+-f ./sql/universal/tables/attribute.sql \
+-f ./sql/universal/tables/entry.sql \
+-f ./sql/universal/tables/entry_attribute.sql \
+-f ./sql/universal/data/attributes.sql \
+-f ./sql/postsetup.sql;
 
 # Insert the trusted key env variable into the admin.trusted_keys table.
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d vault <<-EOSQL
